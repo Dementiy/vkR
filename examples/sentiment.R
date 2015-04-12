d3fadc34202a9f5a226e3204eedfa8800d006ce3
Dementiy <Dementiy@yandex.ru>
2015-04-12 16:03:29 +0300

    Add getSentiment() function

---
+++ examples/sentiment.R
@@ -0,0 +1,13 @@
+##############################################
+### Определение тональности сообщений
+##############################################
+
+# Сентимент-анализ переданного сообщения
+getSentiment <- function(msg, keywords='', key='') {
+  response <- POST('https://russiansentimentanalyzer.p.mashape.com/ru/sentiment/polarity/json/', 
+                   add_headers("X-Mashape-Key" = key, "Content-Type" = "application/json"),
+                   body = paste("{\"text\":\"", msg, "\", 
+                                \"object_keywords\":\"", keywords, "\", 
+                                \"output_format\":\"json\"}", sep=''))
+  content(response, type='application/json', encoding = 'UTF-8')
+}
