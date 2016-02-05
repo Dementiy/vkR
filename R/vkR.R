41fd27414199fb089d146c2fa15c850d9ee7006e
Dementiy <Dementiy@yandex.ru>
2016-02-06 02:09:46 +0300

    Add getAPIVersion() function

--- R/vkR.R
+++ R/vkR.R
@@ -1,4 +1,13 @@
 if (!exists(".vkr")) {
   .vkr <- new.env()
   .vkr$aceess_token <- NULL
+  .vkr$api_version <- '5.44'
+}
+
+getAPIVersion <- function() {
+  .vkr$api_version
+}
+
+setAPIVersion <- function(new_version) {
+  .vkr$api_version <- new_version
 }
