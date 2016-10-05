f97bf1ec5659c0c4e6d33dd14e287f58b709b937
Dmitriy <Dementiy>
2016-10-05 23:29:51 +0300

    Fix typo

--- R/users.R
+++ R/users.R
@@ -447,7 +447,7 @@ getUsersExecute <- function(users_ids='', fields='', name_case='', flatten=FALSE
   if (isTRUE(flatten))
     all_users <- jsonlite::flatten(all_users)
   
-  class(response) <- c(class(response), "vk.users")
+  class(all_users) <- c(class(all_users), "vk.users")
   all_users
 }
 
