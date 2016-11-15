503a25e43261e5dd0624d76fead05add54962235
Dmitriy <Dementiy@yandex.ru>
2016-11-15 23:25:54 +0300

    Update error handling functions

--- R/queries.R
+++ R/queries.R
@@ -13,6 +13,73 @@ request_delay <- function()
 }
 
 
+#' Get error code from response
+has_error <- function(response) {
+  return(ifelse(!is.null(response$error), response$error$error_code, 0))
+}
+
+
+#' Repeat last function call
+repeat_last_query <- function(params = list(), n = 1) {
+  parent_name <- deparse(sys.calls()[[sys.nframe()-n]])
+  parent_args <- as.list(sys.frame(-n))
+  for (arg in names(params))
+    parent_args[[arg]] <- params[[arg]]
+  do.call(what = gsub("\\(.*\\)", "", parent_name), args = parent_args)
+}
+
+
+#' Check response for errors
+try_handle_error <- function(response) {
+  if (!has_error(response))
+      return(NULL)
+  
+  handle_captcha <- function(error) {
+    if (!require("jpeq")) stop("The package jpeg was not installed")
+    download.file(url = error$captcha_img, destfile = 'captcha.jpg', mode = 'wb')
+    captcha_img <- readJPEG("captcha.jpg", native = TRUE)
+    plot(0:1, 0:1, type = "n", ann = FALSE, axes = FALSE)
+    rasterImage(captcha_img, 0, 0, 1, 1)
+    captcha_sid <- error$captcha_sid
+    captcha_key <- readline("Enter the key from captcha: ")
+    return(repeat_last_query(params = list('captcha_key' = captcha_key, 'captcha_sid' = captcha_sid), n = 3))
+  }
+  
+  
+  handle_validation <- function(error) {
+    response <- httr::GET(error$redirect_uri)
+    authorize_form <- XML::htmlParse(rawToChar(response$content))
+    action <- XML::xpathSApply(authorize_form, "//form", XML::xmlGetAttr, "action")
+    if (length(action) != 0 && grepl("security_check", action)) {
+      phone <- XML::xpathSApply(authorize_form, "//*/span", XML::xmlValue)
+      print(phone)
+      missing_numbers <- readline("Enter the missing numbers in the phone number: ")
+      response <- httr::GET(paste0("https://m.vk.com",action), 
+                            query = list('code' = missing_numbers), 
+                            httr::add_headers('Content-Type' = 'application/x-www-form-urlencoded'))
+      for (i in 1:length(response$all_headers)) {
+        location <- response$all_headers[[i]]$headers$location
+        if (!is.null(location) & grepl("access_token", location)) {
+          access_token <- gsub(".*?access_token=(.*?)&.*", "\\1", location)
+          setAccessToken(access_token)
+          return(repeat_last_query(n = 3))
+        }
+      }
+    }
+  }
+  
+  
+  # Handle errors
+  if (has_error(response) == 14) {
+    return(handle_captcha(response$error))
+  } else if (has_error(response) == 17) {
+    return(handle_validation(response$error))
+  } else {
+    stop(response$error$error_msg)
+  }
+}
+
+
 #' Returns a query string
 #'
 #' @param method_name Method name
@@ -48,22 +115,8 @@ execute <- function(code, params = list()) {
   post_res <- httr::POST(url = query, body = append(body, params))
   response <- jsonlite::fromJSON(rawToChar(post_res$content))
   
-  if (!is.null(response$error)) {
-    if (response$error$error_code == 14) {
-      # Captcha handling
-      if (!require("jpeq")) stop("The package jpeg was not installed")
-      download.file(url = response$error$captcha_img, destfile = 'captcha.jpg', mode = 'wb')
-      captcha_img <- readJPEG("captcha.jpg", native = TRUE)
-      plot(0:1, 0:1, type = "n", ann = FALSE, axes = FALSE)
-      rasterImage(captcha_img, 0, 0, 1, 1)
-      
-      captcha_sid <- response$error$captcha_sid
-      captcha_key <- readline("Enter the key from captcha: ")
-      return(execute(code, params = list('captcha_key' = captcha_key, 'captcha_sid' = captcha_sid)))
-    } else {
-      stop(paste0('Server side error: ', response$error$error_msg))
-    }
-  }
+  if (has_error(response))
+    return(try_handle_error(response))
   
   response$response
 }
