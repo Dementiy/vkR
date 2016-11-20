1ed7af9b15f5fd3fdc4d06336d9d08c1aab1ba05
Dmitriy <Dementiy@yandex.ru>
2016-11-20 17:18:33 +0300

    Update db functions

--- R/mongo_connection.R
+++ R/mongo_connection.R
@@ -10,6 +10,54 @@ db_init <- function(db_name = "temp") {
 }
 
 
+#' Get connection
+#' @param collection_name Collection name
+#' @param collection_type Collection suffix
+#' @export
+db_get_connection <- function(collection_name, collection_type) {
+  meta_conn <- db_metaConnection()
+  dbs <- meta_conn$find()
+  dbs <- subset(dbs, db == db_getActive() & collection == collection_name & type == collection_type)
+  
+  if (nrow(dbs) == 0)
+    stop("Collection doesn't exist", call. = FALSE)
+  if (nrow(dbs) > 1)
+    stop("Oops, something has gone wrong. See show_collections()", call. = FALSE)
+  
+  conn <- mongolite::mongo(db = db_getName(),
+                           collection = paste(dbs$db, dbs$collection, dbs$type, sep = '.'))
+  return(conn)
+}
+
+
+#' Get collection
+#' @param collection_name Collection name
+#' @param collection_type Collection suffix
+#' @export
+db_get_collection <- function(collection_name, collection_type) {
+  tryCatch({
+    conn <- db_get_connection(collection_name, collection_type)
+    collection_data <- conn$find()
+    rm(conn)
+    return(collection_data)
+  }, error = function(e) {
+    return(NULL)
+  })
+}
+
+
+#' Import collection from db to global env
+#' @param collection_name Collection name
+#' @param collection_type Collection suffix
+#' @export
+db_load_collection <- function(collection_name, collection_type) {
+  collection <- db_get_collection(collection_name, collection_type)
+  if (is.null(collection)) stop("Collection doesn't exist", call. = FALSE)
+  assign(paste(db_getActive(), collection_name, collection_type, sep = '.'),
+         collection, envir = .GlobalEnv)
+}
+
+
 #' Save object to db
 #' @param object Object to save
 #' @param db_name Database name
@@ -113,17 +161,7 @@ db_drop_collection <- function(collection_name, collection_type) {
   if (missing(collection_name)) stop('argument "collection_name" is missing, with no default')
   if (missing(collection_type)) stop('argument "collection_type" is missing, with no default')
   
-  meta_conn <- db_metaConnection()
-  dbs <- meta_conn$find()
-  dbs <- subset(dbs, db == db_getActive() & collection == collection_name & type == collection_type)
-  
-  if (nrow(dbs) == 0)
-    stop("Collection doesn't exist", call. = FALSE)
-  if (nrow(dbs) > 1)
-    stop("Oops, something has gone wrong, too many collection to drop", call. = FALSE)
-  
-  conn <- mongolite::mongo(db = db_getName(),
-                           collection = paste(dbs$db, dbs$collection, dbs$type, sep = '.'))
+  conn <- db_get_connection(collection_name, collection_type)
   conn$drop()
   rm(conn)
   meta_conn$remove(sprintf('{"db": "%s", "collection": "%s", "type": "%s"}', 
