d44e3723eebc0ec537dfcb79dc1e39b09e01e421
Dmitriy <Dementiy@yandex.ru>
2016-12-02 00:02:49 +0300

    Add short description about package

--- R/vkR.R
+++ R/vkR.R
@@ -1,3 +1,31 @@
+#' Access to VK API via R
+#'
+#' This package provides a series of functions that allow R users
+#' to access VK's API (\url{https://vk.com/dev/methods}) to get
+#' information about users, messages, groups, posts and likes.
+#'
+#' VK (\url{https://vk.com/}) is the largest European online social
+#' networking service, based in Russia. It is available in several
+#' languages, and is especially popular among Russian-speaking users.
+#' VK allows users to message each other publicly or privately, to
+#' create groups, public pages and events, share and tag images,
+#' audio and video, and to play browser-based games [1].
+#'
+#' @references [1] \url{https://en.wikipedia.org/wiki/VK_(social_networking)}
+#'
+#' @seealso \code{\link{vkOAuth}},
+#' \code{\link{getUsersExecute}}, \code{\link{getWallExecute}},
+#' \code{\link{getFriends}}, \code{\link{getFriendsFor}},
+#' \code{\link{getGroupsForUsers}}, \code{\link{getGroupsMembersExecute}},
+#' \code{\link{likesGetListForObjects}}, \code{\link{messagesGetHistoryExecute}},
+#' \code{\link{getArbitraryNetwork}}, \code{\link{getStatus}}
+#'
+#' @name vkR
+#' @docType package
+#' @author Dmitriy Sorokin \email{dementiy@yandex.ru}
+NULL
+
+
 if (!exists(".vkr")) {
   .vkr <- new.env()
   .vkr$aceess_token <- NULL
@@ -15,7 +43,7 @@ getAPIVersion <- function() {
 
 
 #' Set API version
-#' 
+#'
 #' @param v API version
 #' @export
 setAPIVersion <- function(v) {
